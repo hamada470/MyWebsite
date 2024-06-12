4ffc639082f8701598d608ea9ec81faa0ac98751
hamada470 <hamadashapan189@gmail.com>
2024-06-12 19:46:19 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://hamada966533108772.vercel.app/sitemap.xml',
+    sitemap: 'https://mo3lem-ceramic.vercel.app/sitemap.xml',
   }
 }
