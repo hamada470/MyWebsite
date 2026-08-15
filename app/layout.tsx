ba9240c5e42ff2f50fcfbfd2ac670713a6d615a4
hamada470 <hamadashapan189@gmail.com>
2026-08-15 04:47:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,26 +12,44 @@ const arima = Arima({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "تفصيل مغاسل رخام | طاولات رخام | تركيب رخام بالرياض",
-  description: "افضل ورشة تفصيل مغاسل رخام وطاولات رخام طبيعي ومصناعي بالرياض، تركيب وجلي رخام بأعلى جودة للتواصل الاتصال على: 0569962482",
+  description: "افضل ورشة تفصيل مغاسل رخام وطاولات رخام ومصانع رخام بالرياض, تركيب وجلي رخام بأعلى جودة للتواصل الاتصال على: 0569962482",
   keywords: [
-    "تفصيل مغاسل رخام", "طاولات رخام", "تركيب رخام بالرياض", "مغاسل رخام بالرياض", "فني تركيب رخام", 
-    "تفصيل طاولات رخام بالرياض", "معلم رخام الرياض", "رخام طبيعي الرياض", "رخام صناعي الرياض", "مغاسل رخام مودرن", 
-    "مغاسل رخام فخمة", "تفصيل مغاسل فندقية", "طاولات رخام مجالس", "طاولات رخام طعام", "تفصيل طاولات حسب الطلب", 
-    "تركيب واجهات رخام", "تركيب ارضيات رخام", "تركيب درج رخام", "جلي وتلميع رخام", "صيانة مغاسل رخام", 
-    "اسعار مغاسل الرخام الرياض", "معلم مغاسل رخام", "ورشة رخام الرياض", "محل تفصيل رخام", "رخام ميكانيكي بالرياض"
+    "فني تركيب رخام",
+    "مواصل رخام بالرياض",
+    "تركيب رخام بارياض",
+    "طاولات رخام",
+    "تفصيل مغاسل رخام",
+    "مغاسل رخام مودرن",
+    "رخام صناعي الرياض",
+    "رخام طبيعي الرياض",
+    "معلم رخام الرياض",
+    "تفصيل طاولات رخام بارياض",
+    "تفصيل طاولات حسب الطلب",
+    "طاولات رخام طعام",
+    "تفصيل مغاسل فخمة",
+    "مغاسل رخام فخمة",
+    "صيانة مغاسل رخام",
+    "جلي وتلميع رخام",
+    "تركيب ارضيات رخام",
+    "تركيب واجهات رخام",
+    "رخام ميكانيكي بالرياض",
+    "عمل لتفصيل رخام",
+    "ورشة رخام الرياض",
+    "معلم مغاسل رخام",
+    "اسعار مغاسل الرخام الرياض"
   ],
   openGraph: {
     title: "تفصيل مغاسل رخام | طاولات رخام | تركيب رخام بالرياض",
-    description: "افضل ورشة تفصيل مغاسل رخام وطاولات رخام طبيعي ومصناعي بالرياض، تركيب وجلي رخام بأعلى جودة للتواصل الاتصال على: 0569962482",
-    phoneNumbers: ['966569962482'],
+    description: "افضل ورشة تفصيل مغاسل رخام وطاولات رخام ومصانع رخام بالرياض, تركيب وجلي رخام بأعلى جودة للتواصل الاتصال على: 0569962482",
+    phoneNumbers: ["966569962482"],
     siteName: "تفصيل وتركيب رخام بالرياض",
   },
   formatDetection: { telephone: true },
   robots: { index: true, follow: true },
-  referrer: 'origin',
+  referrer: "origin",
   verification: {
-    google: 'nhAv2U5esr02rFZvywbW4j0FcxDrFQDG9xeRia9jJQ',
-  }
+    google: "rhAv2USes0ZrFZvyWBW4JOFcxDrFQD69xeRia9jJQ",
+  },
 };
 
 export default function RootLayout({
@@ -43,6 +61,19 @@ export default function RootLayout({
     <html lang="ar" dir="rtl" translate="no" className="scroll-smooth">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18379909725"
+          strategy="afterInteractive"
+        />
+        <Script id="google-tag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18379909725');
+          `}
+        </Script>
       </head>
       <body className={arima.className}>
         <SpeedInsights />
